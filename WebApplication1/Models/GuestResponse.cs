b6966de5b3ff243af148d212b321cda1771a4a4d
Pawel-Witkowski <witkowski.pawel@hotmail.com>
2017-11-11 19:15:15 +0100

    RSVP
    RSVP system gathering basic data from user and answer if he's atteing
    the party or not.
    
    E-mail not working.

---
+++ WebApplication1/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models {
+    public class GuestResponse {
+        [Required(ErrorMessage = "Prosze podac imie i nazwisko")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Prosze podac poprawny email")]
+        [RegularExpression(".+\\@.+\\..+")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Prosze podac poprawny numer telefonu")]
+        [RegularExpression("[0-9]{9,}")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "No wez powiedz czy przyjdziesz")]
+        public bool?  WillAttend { get; set; }
+    }
+}
